dbafdc44b3b57030108c7329c6cd63b67ccfb51a
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-11-11 18:51:43 +0200

    step1

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Siirrämme tässä osassa fokuksen backendiin, eli palvelimella olevaan toiminnallisuuteen.
+ 
+## 3.1 puhelinluettelon backend step1
+
+Tee Node-sovellus, joka tarjoaa osoitteessa http://localhost:3001/api/persons kovakoodatun taulukon puhelinnumerotietoja:
+
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let persons = [
     {
@@ -23,9 +24,12 @@ let persons = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(persons))
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
 const port = 3001
