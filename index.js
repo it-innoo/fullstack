1cb052493be9620f7966359570d077b632405bbf
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-11-12 12:48:37 +0200

    3.9 puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 
 app.use(bodyParser.json())
-// app.use(morgan('tiny'))
+app.use(cors())
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
